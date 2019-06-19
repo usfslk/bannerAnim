83fa71102047a918c0c1124235225cd10e902d37
Youssef Selkani <ynakles@gmail.com>
2019-06-19 20:47:14 +0100

    deploy

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.endDate = new Date("Jun 19, 2019 20:33:00").getTime();
+window.endDate = new Date("Sep 19, 2019 00:00:00").getTime();
 window.duration = 1000;
 window.easing = 'easeOutQuart';
--- timer.js
+++ timer.js
@@ -1,11 +1,11 @@
 // Youssef Selkani
 // 2019
 
+let now = new Date().getTime();
 function timerLoop() {
-   console.log('launched')
-   let now = new Date().getTime();
    let t = endDate - now;
    if (t >= 0) {
+
       let days = Math.floor(t / (1000 * 60 * 60 * 24));
       let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
@@ -15,33 +15,28 @@ function timerLoop() {
       for (i = 0; i < oldDays.length; i++) {
          oldDays[i].innerHTML = days + "<br/><h6 class='time-label'>Days</h6>";
       }
-
       oldHours = document.getElementsByClassName("hours");
       for (i = 0; i < oldHours.length; i++) {
          oldHours[i].innerHTML = ("0" + hours).slice(-2) + "<br/><h6 class='time-label'>Hours </h6>";
       }
-
       oldMinutes = document.getElementsByClassName("minutes");
       for (i = 0; i < oldMinutes.length; i++) {
          oldMinutes[i].innerHTML = ("0" + mins).slice(-2) + "<br/><h6 class='time-label'>Minutes </h6>";
       }
-
       oldSeconds = document.getElementsByClassName("seconds");
       for (i = 0; i < oldSeconds.length; i++) {
          oldSeconds[i].innerHTML = ("0" + secs).slice(-2) + "<br/><h6 class='time-label'>Seconds </h6>";
       }
-
       OldStatus = document.getElementsByClassName("status");
       for (i = 0; i < OldStatus.length; i++) {
          OldStatus[i].innerHTML = "coming soon";
       }
-
       OldDots = document.getElementsByClassName("dots");
       for (i = 0; i < OldDots.length; i++) {
          OldDots[i].innerHTML = ":";
       }
-
-   } else {
+   }
+   else {
       OldStatus = document.getElementsByClassName("status");
       for (i = 0; i < OldStatus.length; i++) {
          OldStatus[i].innerHTML = "The countdown is over.";
