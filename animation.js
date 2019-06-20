69bc6a18e0d98c32f34bae612114839e9e88352b
Youssef Selkani <ynakles@gmail.com>
2019-06-20 17:30:53 +0100

    fix days

--- animation.js
+++ animation.js
@@ -18,9 +18,7 @@ function toTop() {
    banner.style.setProperty('--clip3', 0);
    banner.style.setProperty('--clip4', '100%');
    banner.style.setProperty('--clip5', 0);
-   banner.style.setProperty('--clip6', '100%');
    banner.style.setProperty('--clip7', '100%');
-   banner.style.setProperty('--clip8', '100%');
    nAnimate.start(duration, function (rate) {
       timerLoop();
       let position = -100 + (rate * 100);
@@ -57,10 +55,12 @@ function toRight() {
 }
 
 function toBottom() {
+   banner.style.setProperty('--clip1', '100%');
    banner.style.setProperty('--clip2', 0);
    banner.style.setProperty('--clip3', 0);
    banner.style.setProperty('--clip4', 0);
    banner.style.setProperty('--clip5', 0);
+   banner.style.setProperty('--clip7', '100%');
    nAnimate.start(duration, function (rate) {
       timerLoop();
       let position = 0 + (rate * 100);
@@ -82,12 +82,17 @@ function toLeft() {
    nAnimate.start(duration, function (rate) {
       timerLoop();
       let position = -100 + (rate * 100);
+      console.log(position)
       banner.style.setProperty('--clip1', Math.abs(position) + "%");
       banner.style.setProperty('--clip7', Math.abs(position) + "%");
       if (Math.abs(position) === 0) {
          let now = new Date().getTime();
          let t = endDate - now;
          if (t >= 0) {
+            banner.style.setProperty('--clip1', '100%');
+            banner.style.setProperty('--clip2', '100%');
+            banner.style.setProperty('--clip3', 0);
+            banner.style.setProperty('--clip4', '100%');
             toTop();
          } else {
             timerLoop();
--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.endDate = new Date("Jun 20, 2019 01:55:00").getTime();
+window.endDate = new Date("Jun 21, 2020 18:38:00").getTime();
 window.duration = 1000;
 window.easing = 'easeOutQuart';``
