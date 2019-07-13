e6eac4a00c96308b63e1f08b0bbb8fff204a9de2
usfslk <macbookair@usfslks-MacBook-Air.local>
2019-07-13 14:16:22 +0100

    update countdown final

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.endDate = new Date("Jul 25, 2019, 23:18:00").getTime();
+window.endDate = new Date("Jul 14, 2019, 23:18:00").getTime();
 window.duration = 1000;
 window.easing = 'easeOutQuart';``
