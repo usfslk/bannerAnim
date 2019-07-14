9464c739697159dce947c917653a402a4267bd3e
usfslk <macbookair@usfslks-MacBook-Air.local>
2019-07-14 21:13:51 +0100

    2020

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.endDate = new Date("Aug 14, 2019, 23:18:00").getTime();
+window.endDate = new Date("Aug 14, 2020, 23:18:00").getTime();
 window.duration = 1000;
 window.easing = 'easeOutQuart';``
